7c0af108eb9c583a734032804a451d0e4723fb13
JarrettR <jrainier@gmail.com>
2021-03-11 20:33:18 -0800

    New IDF project

---
+++ plantlapse-app/main/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+
+
+
+void app_main(void)
+{
+    printf("App begin\n");
+}
