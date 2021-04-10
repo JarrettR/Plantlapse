a097aef134ab7ce62ea57a6f07ab5c42825dac6c
JarrettR <jrainier@gmail.com>
2021-04-09 19:54:42 -0700

    improved OTA resiliance

--- plantlapse-app/main/main.cpp
+++ plantlapse-app/main/main.cpp
@@ -85,43 +85,43 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 
 void ota_task(void *pvParameter)
 {
-    while(plSettings.ota_start == false) {
+    while(1) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    ESP_LOGI(TAG, "Starting OTA");
+        if(plSettings.ota_start == true) {
+        
+            ESP_LOGI(TAG, "Starting OTA");
 
-    esp_http_client_config_t config = {
-        .url = CONFIG_FIRMWARE_UPGRADE_URL,
-        .cert_pem = (char *)server_cert_pem_start,
-        .event_handler = _http_event_handler,
-    };
+            esp_http_client_config_t config = {
+                .url = CONFIG_FIRMWARE_UPGRADE_URL,
+                .cert_pem = (char *)server_cert_pem_start,
+                .event_handler = _http_event_handler,
+            };
 
-#ifdef CONFIG_FIRMWARE_UPGRADE_URL_FROM_STDIN
-    char url_buf[OTA_URL_SIZE];
-    if (strcmp(config.url, "FROM_STDIN") == 0) {
-        example_configure_stdin_stdout();
-        fgets(url_buf, OTA_URL_SIZE, stdin);
-        int len = strlen(url_buf);
-        url_buf[len - 1] = '\0';
-        config.url = url_buf;
-    } else {
-        ESP_LOGE(TAG, "Configuration mismatch: wrong firmware upgrade image url");
-        abort();
-    }
-#endif
+        #ifdef CONFIG_FIRMWARE_UPGRADE_URL_FROM_STDIN
+            char url_buf[OTA_URL_SIZE];
+            if (strcmp(config.url, "FROM_STDIN") == 0) {
+                example_configure_stdin_stdout();
+                fgets(url_buf, OTA_URL_SIZE, stdin);
+                int len = strlen(url_buf);
+                url_buf[len - 1] = '\0';
+                config.url = url_buf;
+            } else {
+                ESP_LOGE(TAG, "Configuration mismatch: wrong firmware upgrade image url");
+                abort();
+            }
+        #endif
 
-#ifdef CONFIG_SKIP_COMMON_NAME_CHECK
-    config.skip_cert_common_name_check = true;
-#endif
+        #ifdef CONFIG_SKIP_COMMON_NAME_CHECK
+            config.skip_cert_common_name_check = true;
+        #endif
 
-    esp_err_t ret = esp_https_ota(&config);
-    if (ret == ESP_OK) {
-        esp_restart();
-    } else {
-        ESP_LOGE(TAG, "Firmware upgrade failed");
-    }
-    while (1) {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+            esp_err_t ret = esp_https_ota(&config);
+            if (ret == ESP_OK) {
+                esp_restart();
+            } else {
+                ESP_LOGE(TAG, "Firmware upgrade failed");
+            }
+        }
     }
 }
 
